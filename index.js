072455ed68bfd66489321036783cb228447a863f
Adrian Kiptoo <cherukip.adrian@gmail.com>
2025-03-05 11:53:55 +0300

    b4power off

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 6.2;
+let num2 = 10;
+let multiply = num1 * num2;
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+let random;
+Math.floor(Math.random()*10) + 1;
+Math.max(1, 2, 3, 5, 4, 6, 7, 20);
